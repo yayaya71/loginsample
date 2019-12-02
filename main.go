c6b80d344c88c2994e2f20af8d9ab1fc285e1a78
yayaya71 <56799548+yayaya71@users.noreply.github.com>
2019-12-03 03:30:49 +0900

    Create main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+
+    "net/http"
+
+    "github.com/gin-gonic/gin"
+
+)
+
+func main() {
+
+    router := gin.Default()
+
+    router.LoadHTMLGlob("views/*.html")
+
+    router.Static("/assets", "./assets")
+
+
+
+    router.GET("/", func(ctx *gin.Context){
+
+        ctx.HTML(http.StatusOK, "index.html", gin.H{})
+
+    })
+
+    router.Run(":8080")
+
+}
