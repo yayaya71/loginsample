1b2500191ce2e73793d9b9f095bbf412ccf7c18f
yayaya71 <ya-ya-ya.71.-_-@docomo.ne.jp>
2020-01-15 02:38:24 +0900

    mysql接続お試し

--- main.go
+++ main.go
@@ -2,12 +2,44 @@ package main
 
 import (
 	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type User struct {
+	ID   int
+	Name string
+}
 
 func main() {
 
+	db, err := sql.Open("mysql", "ya_k/unit2_db")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+ 
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+ 
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.ID, &user.Name)
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(user.ID, user.Name)
+	}
+ 
+	err = rows.Err()
+	if err != nil {
+		panic(err.Error())
+	}
 	router := gin.Default()
 	router.LoadHTMLGlob("views/*.html")
 	router.Static("/assets", "./assets")
